d643d0e8213199daa04cda719d813e4dce5f8372
Giovanna Betti <giovannabetti@gmail.com>
2021-07-13 00:06:15 -0300

    Parte 02 okay - seguindo gabarito do course

--- exercício-2-piadas-chucknorris/index.js
+++ exercício-2-piadas-chucknorris/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const { getCategories, redirectToCategories } = require('./controllers/categorias');
+const { randomJoke, jokeByCategory } = require('./controllers/jokesController');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+app.get('/jokes', randomJoke);
+app.get('/jokes/:category', jokeByCategory);
 
 app.get('/categories', getCategories);
 app.get('/', redirectToCategories);
